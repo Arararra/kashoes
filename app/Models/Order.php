131b74317688616cd459bcf84dcbf3b4d16a05e3
Arararra <55007364+Arararra@users.noreply.github.com>
2026-02-24 21:01:00 +0700

    feat: create order and service model

---
+++ app/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'customer',
+        'customer_name',
+        'customer_phone',
+        'customer_address',
+        'service',
+        'estimated_date',
+        'quantity',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer')->where('role', 'customer');
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class, 'service');
+    }
+}
---
+++ database/migrations/2026_02_24_133548_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer');
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->text('customer_address');
+            $table->unsignedBigInteger('service');
+            $table->date('estimated_date');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('customer')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('service')->references('id')->on('services')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
